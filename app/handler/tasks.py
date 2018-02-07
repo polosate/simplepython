09ecb8830e66997e50404d4c0f4fd69d4f75c52d
polosate <polosate@gmail.com>
2018-02-07 15:21:57 +0300

    tasks handlers

--- app/handler/tasks.py
+++ app/handler/tasks.py
@@ -1,5 +1,5 @@
-import names
-from flask import redirect, url_for, jsonify, json
+from json import loads, JSONDecodeError
+from flask import redirect, url_for, jsonify, abort, request, make_response
 from app import app
 from models import Task
 from database import db_session
@@ -12,6 +12,16 @@ def to_json(task):
             'done': task.done}
 
 
+@app.errorhandler(404)
+def not_found(task_id):
+    return make_response(jsonify({'error': 'Task {} not found'.format(task_id)}), 404)
+
+
+@app.errorhandler(400)
+def json_parse_error():
+    return make_response(jsonify({'error': 'Request is not valid'}), 400)
+
+
 @app.route("/v1/tasks/")
 def get_tasks():
     data = Task.query.all()
@@ -21,15 +31,51 @@ def get_tasks():
     return jsonify(tasks)
 
 
-@app.route("/v1/task/<int:task_id>", methods=["GET", "PUT"])
+@app.route("/v1/task/<int:task_id>", methods=["GET"])
 def get_task(task_id):
     task = Task.query.filter_by(id=task_id).first()
+    if task is None:
+        return not_found(task_id)
     return jsonify(to_json(task))
 
 
-@app.route("/v1/task/", methods=["GET"])
+@app.route("/v1/task/<int:task_id>", methods=["PUT"])
+def update_task(task_id):
+    task = Task.query.filter_by(id=task_id).first()
+    if task is None:
+        return not_found(task_id)
+    try:
+        r = loads(request.data)
+    except JSONDecodeError:
+        return "json parsing error"
+
+    name = r.get("name", None)
+    desc = r.get("description", None)
+    done = r.get("done", None)
+
+    if name is None and desc is None and done is None:
+        return "Nothing to update"
+
+    if name is not None:
+        task.name = name
+    if desc is not None:
+        task.desc = desc
+    if done is not None:
+        task.done = done
+    db_session.commit()
+    return make_response(jsonify(True), 200)
+
+
+@app.route("/v1/task/", methods=["POST"])
 def create_task():
-    new_task = Task(names.get_first_name(), names.get_full_name())
+    try:
+        r = loads(request.data)
+        name = r["name"]
+        desc = r["description"]
+    except (JSONDecodeError, KeyError):
+        return json_parse_error()
+
+    new_task = Task(name, desc)
     db_session.add(new_task)
     db_session.commit()
     task_id = new_task.id
